079d2ac50fa4b47251fd615806ea48d2664b14d8
Zhanatbek <zhanatbeke@gmail.com>
2023-08-07 20:56:22 +0600

    add first lesson

---
+++ LSN1/lesson/main.js
@@ -0,0 +1,51 @@
+// console.log('hello world');
+// alert('uvedomleniye');
+
+
+// confirm('prinyate usloviye');
+// prompt('your name');
+
+var message = 'prinyate usloviye';
+
+// confirm(message);
+// let name = 'eldar';
+// const age = 34;
+// console.log(name, age);
+// name = 'zhanat';
+// console.log(name);
+// prompt('your name');
+
+// let isActive = confirm('avtorizovatsa?');
+// console.log(osActive);
+let string = 'jehbigukjfhew';
+let number = 2454;
+let boolean = true;
+let und = undefined;
+let pppppp;
+console.log(pppppp);
+let n = null;
+
+let obj = {
+    name: 'hrjgerukd',
+    age: 22,
+    child: {},
+
+
+
+};
+let array = [1, 'str', {}, obj, true];
+
+
+// let sum = 2 + 2;
+// console.log(sum);
+// let difference = 5 - 3;
+// console.log(difference);
+// let product = 4 * 3;
+// console.log(product);
+// let quotient = 10 / 2;
+
+// let reminder = 15 % 3;
+// let increment = 7;
+// console.log(increment);
+
+
