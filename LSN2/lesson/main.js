e440acdfc4f6c1746d7cf50df42e690fb74582a9
Zhanatbek <zhanatbeke@gmail.com>
2023-08-14 21:37:56 +0600

    add lessons

---
+++ LSN2/lesson/main.js
@@ -0,0 +1,21 @@
+// let all = 12;
+// if (all === 18) {
+//     alert('equal')
+// }
+// else if (all > 18) {
+//     alert('biiger ')
+// }
+// else {
+//     alert('net')
+// }
+// // !false --> true
+// // !true --> false
+
+// if (all > 18 && all, 30);
+let login = prompt("vedite login");
+let password = prompt("vedite parol");
+
+
+let userName = 'admin';
+let userPassword = '12345qwerty';
+
